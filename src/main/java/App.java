e2070a4a6c3f93b29e455c75014b65c9d2df1b90
Nisha Shankar <nishashankar822@gmail.com>
2026-04-10 02:25:30 +0530

    updated port to 8080 for k8s

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         // HOME PAGE
         server.createContext("/", exchange -> {
